fc326a0655973d7e1055604e751a52b2631ffa00
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-04-03 16:02:49 +0530

    updated

--- ShoppersDenv1/obj/Debug/net8.0/ShoppersDenv1.AssemblyInfo.cs
+++ ShoppersDenv1/obj/Debug/net8.0/ShoppersDenv1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppersDenv1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a529f7cc11376d94bb263e70abe0edd5dc822a20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e1a9f13a7b400d104bdb9f73cb4e997727a783c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppersDenv1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppersDenv1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
